86871f28e62b853029a9593f34712b86a994ba25
Bappy71 <19103076@iubat.edu>
2022-09-15 05:39:39 +0600

    My  commit

--- JS/bank.js
+++ JS/bank.js
@@ -1,4 +1,6 @@
 document.getElementById('Login-submit').addEventListener('click', function () {
+    
+    ///get User Email///
     const emailField = document.getElementById('User-Email');
     const userEmail = emailField.value;
     console.log(userEmail);
@@ -7,6 +9,12 @@ document.getElementById('Login-submit').addEventListener('click', function () {
     ///get User password////
     const passwordField = document.getElementById('User-Password');
     const userPassword = passwordField.value;
-    console.log(userEmail);
+    console.log(userPassword);
+   
+    ///Check Email and Password///
+     
+    if(userEmail == 'bappy@gmail.com' && userPassword == '12345'){
+        window.location.href = "banking.html"
+    }
 
 })
