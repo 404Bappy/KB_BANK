6d091953d291df9656b738789dd9f3d4378ca36b
Bappy71 <19103076@iubat.edu>
2022-09-21 03:28:31 +0600

    My  commit

--- JS/Banking.js
+++ JS/Banking.js
@@ -1,5 +1,14 @@
 //Handle Deposit Submit Event //
 
 document.getElementById("Deposite-submit").addEventListener('click', function(){
-    console.log('Deposit Submitted');
+    //Get the Deposited Amount//
+    const depositInput = document.getElementById('Deposit-amount');
+    const deposiAmount = depositInput.value ;
+    console.log(deposiAmount)
+
+    const totalDeposit = document.getElementById('Total-Deposit');
+   totalDeposit.innerText = deposiAmount;
+
+   //clear the deposit input field//
+   depositInput.value = ' ';
 })
