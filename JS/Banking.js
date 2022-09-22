1374d4a295873bce2a698f5670789ec05a5f1b36
Bappy71 <19103076@iubat.edu>
2022-09-23 01:14:53 +0600

    My  commit

--- JS/Banking.js
+++ JS/Banking.js
@@ -31,5 +31,14 @@ document.getElementById("Deposite-submit").addEventListener('click', function ()
 document.getElementById('withdraw-submit').addEventListener('click', function(){
     const withdrawInput = document.getElementById('Withdraw-amount');
     const withdrawAmountText = withdrawInput.value;
-    console.log(withdrawAmountText);
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+    console.log(newWithdrawAmount);
+
+    //set Withdraw Total Amount //
+
+    const totalWithdrawAmount =  document.getElementById('withdraw-Money');
+    const previousWithdrawText = totalWithdrawAmount.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const
 })
